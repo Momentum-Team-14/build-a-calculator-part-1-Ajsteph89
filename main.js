5f9699b1d0a8b98448d161cec45c19484ac3fbe6
Ajsteph89 <ajsteph89@gmail.com>
2022-07-28 12:09:36 -0400

    added decimal point limiter

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ let operators = document.querySelectorAll('.mathSymb')
 let answer = document.getElementById('calculate')
 let clear = document.getElementById('clearButton')
 
+
 for (let number of numbers){
     number.addEventListener('click', ()=> {
         display.innerText += number.innerText;
+        charLimit();
     });
 }
 for (let operator of operators){
@@ -16,11 +18,17 @@ for (let operator of operators){
 }
 answer.addEventListener('click', ()=> {
     display.innerText = eval(display.innerText);
+    truncate();
 });
 
 clear.addEventListener('click', ()=> {
     display.innerText = '';
 });
+
+function truncate (){
+    display.innerText = +parseFloat(eval(display.innerText)).toFixed(7);
+}
+
 
 
 // function calculate (operator, num1, num2){
